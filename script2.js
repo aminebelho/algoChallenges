2903e89b768d3b97b7ce4f6c733cf525b76c2041
aminebelho21 <belhocineamine21@gmail.com>
2023-09-08 11:28:01 +0100

    2eme

---
+++ script2.js
@@ -0,0 +1,44 @@
+let array = [12, 54,25,48,47,36,29,10]
+
+function even(x) {
+    let newArray = []
+    for(i = 0 ; i < x.length ; i++){
+        if (x[i] % 2 == 0){
+            newArray.push(x[i])
+        }
+    }
+    console.log(newArray)
+}
+
+even(array)
+
+
+let persons = [{name: "person 1", age: 24, email:"email1"}, {name: "person 2", age: 57, email:"email2"}, {name: "person 3", age: 34, email:"email3"}]
+
+function old(x){
+    let old = []
+    for(i = 0; i < x.length; i++){
+        if(x[i].age >= 30){
+            old.push(x[i])
+        }
+    }
+    console.log(old)
+}
+
+old(persons)
+
+const vowels = ["a", "e", "y", "u", "i", "o"]
+function removeVow(x) {
+    for(i = 0 ; i < x.length ; i++){
+        for(j = 0 ; j < vowels.length; j++){
+            if(x[i] == vowels[j]){
+                console.log("vow", x[i])
+                x = x.slice(0, i) + x.slice(i + 1);
+            }
+        }
+    }
+    console.log(x)
+}
+
+let ex = "example"
+removeVow(ex)
